b908f79527d5516008a2cef468f408d9aa08ced3
HariK-developer <hariswdeveloper@gmail.com>
2024-03-15 10:49:39 +0530

    custom mangers

--- guide/models.py
+++ guide/models.py
@@ -92,5 +92,51 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 
+class BankAccount(models.Model):
+    account_number = models.CharField(max_length=255,unique=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    
+
+# Custom Managers 
 
 
+class AuthorManager(models.Manager):
+    def get_queryset(self):
+        """
+        Returns a QuerySet of all objects in the database that have  marked for role A.
+        """
+        return super().get_queryset().filter(role='A')
+
+
+class EditorManager(models.Manager):
+    def get_queryset(self):
+        """
+        Returns a QuerySet of all objects in the database that have  marked for role E.
+        """
+        return super().get_queryset().filter(role='E')
+    
+    
+class People(models.Model):
+    choices = (("A","Author"),("E","Editor"))
+    
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=1,choices=choices)
+    
+    objects = models.Manager() # default manager
+    
+    authors = AuthorManager() # author manager
+    
+    editors = EditorManager() # editor manager
+    
+    
+
+# This will return person whose role is author
+People.authors.all()
+
+# This will return whose role is editor
+People.editors.all()
+
+# This will return all person whose role is either author or editor
+Person.objects.all()
--- guide/views.py
+++ guide/views.py
@@ -1,12 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.messages import constants as messages_constants
+from django.shortcuts import get_object_or_404
+from django.db import transaction
 from .api import critical
-from .models import BankDetails, Book
+from .models import BankAccount, BankDetails, Book, Student
 from .helpers import decrypt_data
 
 # Create your views here.
 
 MESSAGE_LEVEL = messages_constants.DEBUG
+
 
 def fake_data():
     """
@@ -19,9 +23,10 @@ def fake_data():
         None
 
     """
-    x = Book(title='Book3',author_id=3)
+    x = Book(title="Book3", author_id=3)
     x.save()
-    
+
+
 # fake_data()
 
 
@@ -63,4 +68,143 @@ def bank_details(request):
     return render(request, "bank_details.html", context)
 
 
+"""Django Transaction Details"""
 
+
+@transaction.atomic
+def transfer_funds(request, from_account_number, to_account_number, amount):
+    """
+    This function debits an account and credits another account.
+
+    Args:
+        request (HttpRequest): The incoming request object.
+        from_account_number (str): The account number of the source account.
+        to_account_number (str): The account number of the destination account.
+        amount (int): The amount to be transferred.
+
+    Returns:
+        JsonResponse: A JSON response containing a success or error message.
+
+    Raises:
+        ValueError: If the source or destination account does not exist.
+        ValueError: If the source account does not have sufficient funds.
+
+    """
+    try:
+        from_account = get_object_or_404(
+            BankAccount, account_number=from_account_number
+        )
+        to_account = get_object_or_404(BankAccount, account_number=to_account_number)
+        if from_account.balance < amount:
+            return JsonResponse({"error": "Insufficient balance"}, status=400)
+
+        from_account.balance -= amount
+        from_account.save()
+
+        to_account.balance += amount
+        to_account.save()
+
+        return JsonResponse({"message": "Transfer Successful"}, status=200)
+    except Exception as e:
+
+        return JsonResponse({"error": str(e)}, status=500)
+
+
+"""Django Transaction other important concepts"""
+
+
+@transaction.atomic
+def my_transaction_function():
+    """
+    This function demonstrates the use of the Django transaction module.
+
+    This function creates two Student objects, one with the name "David" and another with the name "John". If the creation of the second Student object fails, the transaction is rolled back, and an exception is raised.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If the creation of the second Student object fails.
+
+    """
+    try:
+        stu1 = Student.objects.create(name="David")
+        # create a savepoint
+        std = transaction.savepoint()
+
+        stu2 = Student.objects.create(name="John")
+        # Roll back to the savepoint
+        transaction.savepoint_rollback(std)
+
+    except Exception as e:
+        # Roll back entire transaction in case of an exception
+        transaction.set_rollback(True)
+
+
+"""Django Transaction other important concepts 
+    commit :- It makes all changes permanent to the database
+"""
+
+
+@transaction.atomic
+def my_transaction_commit():
+    """
+    This function demonstrates the use of the Django transaction module.
+
+    This function creates two Student objects, one with the name "David" and another with the name "John". If the creation of the second Student object fails, the transaction is rolled back, and an exception is raised.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If the creation of the second Student object fails.
+
+    """
+    try:
+        stu1 = Student.objects.create(name="David")
+
+        # finalize the transaction by committing the changes
+        transaction.commit()
+
+    except Exception as e:
+        # Roll back entire transaction in case of an exception
+        transaction.set_rollback(True)
+
+
+"""Django Transaction other important concepts 
+    Nested Transactions :- Using context manager we can even implement nested transactions
+"""
+
+
+@transaction.atomic
+def my_nested_transaction():
+    """
+    This function demonstrates the use of the Django transaction module.
+
+    This function creates two Student objects, one with the name "David" and another with the name "John". If the creation of the second Student object fails, the transaction is rolled back, and an exception is raised.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If the creation of the second Student object fails.
+
+    """
+    stu1 = Student.objects.create(name="David")
+
+    # finalize the transaction by committing the changes
+    transaction.commit()
+
+    with transaction.atomic():
+        # perform nested transactions
+        stu2 = Student.objects.create(name="John Doe")
+   
